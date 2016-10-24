bddee6ab9e15766127e6270ea40275a1e16910d2
KristenChing <kristenching1@gmail.com>
2016-10-24 10:17:30 -0700

    replaced BMI Part 2

--- Lab_05/Lab_05.1/EX_02_BMI_Part2.py
+++ Lab_05/Lab_05.1/EX_02_BMI_Part2.py
@@ -5,18 +5,19 @@ weight = float(input("Please enter your weight (in pounds/lbs) "))
 def calcBMI(h, w):
     global BMI
     BMI = w*0.45/((h*0.025)**2)
-    if BMI < 18.5:
-        return "Underweight"
-    if (BMI > 18.5) and (BMI < 24.9):
-        return "Normal"
-    if (BMI > 25) and (BMI <29.9 ):
-        return "Overweight"
-    if (BMI > 29.9) and (BMI < 34.9):
-        return "Obese"
-    if (BMI > 35) and (BMI < 39.9):
-       return "Very Obese"
-    if (BMI > 39.9):
+    if BMI >= 39.9:
         return "Morbidly Obese"
+    elif BMI >= 35:
+        return "Very Obese"
+    elif BMI >= 29.9:
+        return "Obese"
+    elif BMI >= 25:
+        return "Overweight"
+    elif BMI > 18.5:
+        return "Normal"
+    elif BMI < 18.5:
+        return "Underweight"
+        
 condition = calcBMI(height, weight)
 print("Your BMI is:", BMI)
 print("You are", condition)
