904c320a8ddaca1efb0f8c019430ff159ac94345
KristenChing <kristenching1@gmail.com>
2016-10-24 10:05:24 -0700

    replaced lab_04 compund interest

--- Lab_04/Lab_04.1/Ex_03_Compund_Interest.py
+++ Lab_04/Lab_04.1/Ex_03_Compund_Interest.py
@@ -1,11 +1,11 @@
 #Kristen Ching
-r = int(input("Enter the interest rate. "))
-p = int(input("Enter the principal. "))
-n = int(input("Enter the number of times the loan is compounded per year. "))
-t = int(input("Enter the life of the loan (in years). "))
+r = float(input("Enter the interest rate. "))
+p = float(input("Enter the principal. "))
+n = float(input("Enter the number of times the loan is compounded per year. "))
+t = float(input("Enter the life of the loan (in years). "))
 
 def payment(r, p, n, t):
-    return p*(1 + r/n)**(n*t)
+    return p*((1 + r/n)**(n*t))/(12*t)
 
 cost = payment(r, p, n, t)
-print("The total cost of the loan is", cost) 
+print("The total payment of the loan is ${:0.2f}".format(cost)) 
