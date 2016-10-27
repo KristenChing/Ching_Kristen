9b399acfe3d7cc9717b8a473bfc0742a44b77ed8
KristenChing <kristenching1@gmail.com>
2016-10-27 10:13:00 -0700

    fixed song.py

--- Lab_06/Lab_06.1/EX_08_Song.py
+++ Lab_06/Lab_06.1/EX_08_Song.py
@@ -1,13 +1,13 @@
 #Kristen Ching
-output = ""
-word = ""
-def sing(output, word, word1):
-    for x in range(0, 4):
-            word = word1
+def sing(word, num):
+    global output
+    output = ""
+    for x in range(0, num):
             output = output + word + " "
-            if x == 3:
+            if x == (num - 1):
                 print(output)
-                print(output)
-sing(output, word, "Na")
-print("Hey "*3)
-print("Goodbye!")
+sing("Na", 4)
+sing("Na", 4)
+sing("Hey", 3)
+sing("Goodbye!", 1)
+
