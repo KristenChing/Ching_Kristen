8f683445fe7c54e8bdcb6fbd1a5dbde825939a95
KristenChing <kristenching1@gmail.com>
2016-11-17 10:11:19 -0800

    corrected underscores

--- Lab_08/EX_01_Underscores.py
+++ Lab_08/EX_01_Underscores.py
@@ -1,8 +1,9 @@
 #Kristen Ching
 sentence = input("Please enter a sentence. ")
-def replace(sentence):
-    if " " not in sentence:
-        return sentence
+def replace(sc):
+    if " " not in sc:
+        return sc
     else:
-        return replace(sentence.replace(" ", "_"))
+       return sc[0:sc.index(" ")] + "_" + replace(sc[sc.index(" ")+1:len(sc)])
+
 print(replace(sentence))
