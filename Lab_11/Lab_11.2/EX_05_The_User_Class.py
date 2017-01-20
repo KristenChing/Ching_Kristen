61c090e56335b2f99a6a2debb88d426d82f47a83
KristenChing <ChingK3594@sduhsd.lan>
2017-01-20 10:20:27 -0800

    Finished Lab_11.2 !!! yaaaaaa

---
+++ Lab_11/Lab_11.2/EX_05_The_User_Class.py
@@ -0,0 +1,36 @@
+#Kristen Ching
+import random
+class User:
+    def __init__(self, firstName, lastName, avat=""):
+        self.first = firstName
+        self.last = lastName
+        self.avatar = avat
+        self.ID = random.randint(0, 100000)
+
+    def setVals(self, firstName, lastName, avat):
+        self.first = firstName
+        self.last = lastName
+        self.avatar = avat
+        self.ID = random.randint(0, 100000)
+
+    def __str__(self):
+        return "Customer Info... \nFirst Name: " + self.first + \
+        "\nLast Name: " + self.last + \
+        "\navatar: " + self.avatar + \
+        "\nUser ID: " + str(self.ID)
+
+def main():
+    firstName = input("Please enter your first name. ")
+    lastName = input("Please enter your last name. ")
+    createAvat = input("Would you like to use a public avatar? Enter y/n ")
+    if createAvat == "y":
+        avat = input("Please enter a name for your public avatar. ")
+        user1 = User(firstName, lastName)
+        user1.setVals(firstName, lastName, avat)
+    else:
+        user1 = User(firstName, lastName)
+
+    #user1.setVals(firstName, lastName, avat)
+    print(user1.__str__())
+    
+main()
---
+++ Lab_11/Lab_11.2/EX_06_Inventory_Items.py
@@ -0,0 +1,32 @@
+#Kristen Ching
+class Inventory:
+    def __init__(self, manuf, name, category = "", price = ""):
+        self.manufacturer = manuf
+        self.name = name
+        self.category = category
+        self.price = price
+
+    def resetVals(self, manuf, name, category, price):
+        self.manufacturer = manuf
+        self.name = name
+        self.category = category
+        self.price = price
+
+    def __str__(self):
+        return "Item Info... \nManufacturer: " + self.manufacturer + \
+        "\nName: " + self.name + \
+        "\nCategory: " + self.category + \
+        "\nPrice: $" + str(self.price)
+def main():
+    manuf = input("Please enter manufacturer name. ")
+    name = input("Please enter the name of the item. ")
+    moreInfo = input("Do you wish to enter category and price information? Enter y/n ")
+    if moreInfo == "y":
+        item1 = Inventory(manuf, name)
+        category = input("Please enter the category of the item. ")
+        price = input("Please enter the price of the item. ")
+        item1.resetVals(manuf, name, category, price)
+    else:
+        item1 = Inventory(manuf, name)
+    print(item1.__str__())
+main()
