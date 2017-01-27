b68996ace00c7fc34e59fc90e92f782c395e3ba4
KristenChing <ChingK3594@sduhsd.lan>
2017-01-27 10:09:04 -0800

    added upc

--- Lab_11/Lab_11.2/EX_06_Inventory_Items.py
+++ Lab_11/Lab_11.2/EX_06_Inventory_Items.py
@@ -1,8 +1,10 @@
 #Kristen Ching
+import random
 class Inventory:
     def __init__(self, manuf, name, category = "", price = ""):
         self.manufacturer = manuf
         self.name = name
+        self.upc = random.randint(0, 10000)
         self.category = category
         self.price = price
 
@@ -11,12 +13,14 @@ class Inventory:
         self.name = name
         self.category = category
         self.price = price
+        self.upc = random.randint(0, 10000)
 
     def __str__(self):
         return "Item Info... \nManufacturer: " + self.manufacturer + \
         "\nName: " + self.name + \
         "\nCategory: " + self.category + \
-        "\nPrice: $" + str(self.price)
+        "\nPrice: $" + str(self.price) + \
+        "\nUPC: #" + str(self.upc)
 def main():
     manuf = input("Please enter manufacturer name. ")
     name = input("Please enter the name of the item. ")
