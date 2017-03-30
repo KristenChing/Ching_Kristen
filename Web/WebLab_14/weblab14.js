8085be3c68f44a5f396ee5c22cf8218091a5a980
KristenChing <kristenching1@gmail.com>
2017-03-30 09:37:32 -0700

    lllllllllll

---
+++ Web/WebLab_14/weblab14.js
@@ -0,0 +1,9 @@
+var x = document.form.input.username.value;
+function validate(){
+    document.write(x);
+    //var atVal = x.indexOf("@");
+    
+    // if(atVal < 1 )
+    //     alert("Invalid email.");
+}
+document.write(x);
