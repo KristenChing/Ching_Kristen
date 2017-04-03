9dc211747ba1d1d67965ece7309d13c1e02b2fb3
KristenChing <kristenching1@gmail.com>
2017-04-03 09:24:51 -0700

    iaajjjaa  im done yay

--- Web/WebLab_14/weblab14.js
+++ Web/WebLab_14/weblab14.js
@@ -1,9 +1,15 @@
-var x = document.form.input.username.value;
 function validate(){
-    document.write(x);
-    //var atVal = x.indexOf("@");
-    
-    // if(atVal < 1 )
-    //     alert("Invalid email.");
+    var x = document.forms.input.username.value;
+    var y = document.forms.input.password.value;
+    var atVal = x.indexOf("@");
+    var dotVal = x.lastIndexOf(".");
+    document.write(atVal);
+    document.write(dotVal);
+    if(atVal < 1 || dotVal < atVal + 2)
+        alert("Error: invalid email.");
+    if (y.length < 6)
+        alert("Error: password is too short. Your password must be a minimum of 6 characters!");
+    else if (atVal != -1 || dotVal != -1)
+        alert("Success!")
+
 }
-document.write(x);
