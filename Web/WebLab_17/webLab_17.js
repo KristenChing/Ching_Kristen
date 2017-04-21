82924815f850b3a20d260f2c0cbbacea7ccbb1c2
KristenChing <kristenching1@gmail.com>
2017-04-21 10:16:58 -0700

    i feel proud now LOL

---
+++ Web/WebLab_17/webLab_17.js
@@ -0,0 +1,48 @@
+function drag(){
+    k = document.getElementById("img");
+    leftBox = document.getElementById("left");
+    k.addEventListener("dragstart", start, false);
+    k.addEventListener("dragstart", end, false);
+    leftBox.addEventListener("dragenter", enter, false);
+    leftBox.addEventListener("dragleave",  leave, false);
+    leftBox.addEventListener("dragover", over, false);
+    leftBox.addEventListener("drop", drop, false);
+}
+
+function start (e){
+    var img = '<img id = "img" src = "image.jpg" height = "200" width = "200">';
+    e.dataTransfer.setData("image", img);
+}
+
+function enter (e) {
+    e.preventDefault();
+    leftBox.style.background = "#42d100";
+    leftBox.style.border = "3px solid green";
+
+}
+
+function leave (e){
+    e.preventDefault();
+    leftBox.style.background = "white";
+    leftBox.style.border = "3px solid blue";
+}
+
+function end(e){
+    img = e.target();
+    img.style.visibility = "hidden";
+}
+
+function over(e){
+    e.preventDefault();
+    leftBox.style.background = "#42d100";
+    leftBox.style.border = "3px solid green";
+
+}
+
+function drop(e){
+    e.preventDefault();
+    leftBox.style.background = "white";
+    leftBox.style.border = "3px solid blue";
+    left.innerHTML = e.dataTransfer.getData("image");
+}
+window.addEventListener("load", drag, false);
