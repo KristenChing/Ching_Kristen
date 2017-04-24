b7b3a63b81b8c6d640b15810b853d02b8f90afae
KristenChing <kristenching1@gmail.com>
2017-04-24 10:06:00 -0700

    hehe i fixed it cx

--- Web/WebLab_17/webLab_17.js
+++ Web/WebLab_17/webLab_17.js
@@ -2,7 +2,7 @@ function drag(){
     k = document.getElementById("img");
     leftBox = document.getElementById("left");
     k.addEventListener("dragstart", start, false);
-    k.addEventListener("dragstart", end, false);
+    k.addEventListener("dragend", end, false);
     leftBox.addEventListener("dragenter", enter, false);
     leftBox.addEventListener("dragleave",  leave, false);
     leftBox.addEventListener("dragover", over, false);
@@ -12,6 +12,7 @@ function drag(){
 function start (e){
     var img = '<img id = "img" src = "image.jpg" height = "200" width = "200">';
     e.dataTransfer.setData("image", img);
+    e.target.style.opacity = "0.5";
 }
 
 function enter (e) {
@@ -28,15 +29,13 @@ function leave (e){
 }
 
 function end(e){
-    img = e.target();
-    img.style.visibility = "hidden";
+    e.target.style.visibility = "hidden";
 }
 
 function over(e){
     e.preventDefault();
     leftBox.style.background = "#42d100";
     leftBox.style.border = "3px solid green";
-
 }
 
 function drop(e){
